a96327ea6b346c3dbe53110ccf33515a93ca8c44
Carlos Eduardo de Camargo <cadu.camargoads@gmail.com>
2021-10-17 11:02:57 -0300

    Primeiro commit do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './components/home/home.component';
+import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produto/cadastrar-produto.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'produtos/lista',
+    component: ListarProdutosComponent
+  },
+  {
+    path: 'produtos/cadastrar', component: CadastrarProdutoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
